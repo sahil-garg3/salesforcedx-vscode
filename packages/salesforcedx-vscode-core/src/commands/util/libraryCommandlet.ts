a7c582e710b565d92cf5a98bd1dfd41aa6926576
Ananya <a.jha@salesforce.com>
2020-06-24 17:15:08 -0500

    rearranged and added diagnostics

--- packages/salesforcedx-vscode-core/src/commands/util/libraryCommandlet.ts
+++ packages/salesforcedx-vscode-core/src/commands/util/libraryCommandlet.ts
@@ -4,9 +4,11 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
+import { Connection } from '@salesforce/core';
 import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/out/src/types';
+import { nls } from '../../messages';
 import { TelemetryData, telemetryService } from '../../telemetry';
+import { OrgAuthInfo } from '../../util';
 import { CommandletExecutor } from './sfdxCommandlet';
 
 export abstract class LibraryCommandletExecutor<T>
@@ -34,5 +36,14 @@ export abstract class LibraryCommandletExecutor<T>
     output: string
   ): TelemetryData | undefined {
     return;
+  }
+
+  public async initalizeConnection(): Promise<Connection> {
+    const usernameOrAlias = await OrgAuthInfo.getDefaultUsernameOrAlias(true);
+    if (!usernameOrAlias) {
+      throw new Error(nls.localize('error_no_default_username'));
+    }
+    const conn = await OrgAuthInfo.getConnection(usernameOrAlias);
+    return conn;
   }
 }
